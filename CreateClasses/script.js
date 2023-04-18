340abdb9a2c04711e4481a03ae301c1d97453f97
SJRXD <rohr.olasz@gmail.com>
2023-04-17 17:42:41 -0700

    Adding Class Definition Outline to Notes, might need reorg later

---
+++ CreateClasses/script.js
@@ -0,0 +1,32 @@
+// General Notes - might need reorganization
+
+// Class methods are non-enumerable
+
+/* "In object-oriented programming, a class is an extensible program-code-template for creating objects, 
+providing initial values for state (member variables) and implementations of behavior (member functions or methods)." */
+
+// Non-enumerable properties are those that do not show up in a for...in loop
+
+// https://javascript.info/class#:~:text=Class%20methods%20are%20non%2Denumerable,Classes%20always%20use%20strict%20.
+
+// Define Class
+
+// Class Declaration
+// Class Keyword
+// Class Methods
+// Class Inheritance
+// Class Inheritance with Constructor
+// Class Inheritance with Super
+// Class Inheritance with Super and Constructor
+// Class Inheritance with Super and Constructor and Static
+// Class Inheritance with Super and Constructor and Static and Getters and Setters
+// Class Inheritance with Super and Constructor and Static and Getters and Setters and Static
+// Class Inheritance with Super and Constructor and Static and Getters and Setters and Static and Private
+// Class Inheritance with Super and Constructor and Static and Getters and Setters and Static and Private and Static
+
+// oh my
+
+// Class Prototype
+
+// Class Properties
+
